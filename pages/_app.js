bb20639725dcc1b622912e6c136e8f9047cfa6b6
lolieatapple <lolieatapple@gmail.com>
2022-06-30 00:28:01 +0800

    update sider bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,24 @@
+import { ThemeProvider } from '@emotion/react'
+import { Stack } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
 import '../styles/globals.css'
+import SideBar from './_sideBar'
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  }
+})
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <ThemeProvider theme={theme}>
+    <Stack spacing={2} direction='row'>
+      <SideBar />
+      <div style={{width:'100%', height: "100vh", overflow: "scroll"}}>
+        <Component {...pageProps} />
+      </div>
+    </Stack>
+  </ThemeProvider>
 }
 
 export default MyApp
---
+++ pages/_sideBar.js
@@ -0,0 +1,111 @@
+import { Autocomplete, Button, Divider, List, ListItem, ListItemButton, ListItemIcon, ListItemText, Paper, Stack, styled, TextField } from "@mui/material"
+import AccountBalanceWalletIcon from '@mui/icons-material/AccountBalanceWallet';
+import CollectionsBookmarkIcon from '@mui/icons-material/CollectionsBookmark';
+import SendIcon from '@mui/icons-material/Send';
+import AccountBalanceIcon from '@mui/icons-material/AccountBalance';
+import AbcIcon from '@mui/icons-material/Abc';
+import CurrencyBitcoinIcon from '@mui/icons-material/CurrencyBitcoin';
+import LinkIcon from '@mui/icons-material/Link';
+import { useState } from "react";
+import Head from "next/head";
+import { useRouter } from "next/router";
+
+const Bar = styled("div")`
+  height: 100vh;
+  width: 300px;
+`
+
+
+export default function SideBar() {
+  const [selectedIndex, setSelectedIndex] = useState(-1);
+  const [balance, setBalance] = useState('0');
+  const router = useRouter();
+  
+  return <Bar>
+    <Head>
+      <title>Aries Web Wallet</title>
+      <meta name="description" content="Multi-chain EVM Web Wallet" />
+      <link rel="icon" href="/favicon.png" />
+    </Head>
+    <Paper elevation={0} sx={{height: "100vh"}}>
+      <Stack spacing={0}>
+      <Stack spacing={2} sx={{padding: '20px 15px'}}>
+        <Stack spacing={1} direction='row' sx={{padding: '15px'}}>
+        {/* <AccountBalanceWalletIcon sx={{marginTop:'-3px'}} /> */}
+        <img src="/favicon.png" width={48} style={{marginTop:'-13px', marginRight: '10px'}} />
+        <i>Aries Web Wallet</i>
+        </Stack>
+        <Button variant="outlined">Connect Wallet</Button>
+        <TextField size="small" label="Balance" value={balance} />
+      </Stack>
+      <Divider direction="horizontal" />
+        <List>
+          <ListItem disablePadding>
+            <ListItemButton selected={router.pathname === '/'} onClick={() => setSelectedIndex(-1)} component="a" href="/" >
+              <ListItemIcon>
+                <AccountBalanceIcon />
+              </ListItemIcon>
+              <ListItemText primary="Home" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton selected={router.pathname === '/smart_contract'} onClick={() => setSelectedIndex(0)} component="a" href="/smart_contract" >
+              <ListItemIcon>
+                <AccountBalanceIcon />
+              </ListItemIcon>
+              <ListItemText primary="Smart Contract" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton selected={selectedIndex === 1} onClick={() => setSelectedIndex(1)} component="a" href="/">
+              <ListItemIcon>
+                <SendIcon />
+              </ListItemIcon>
+              <ListItemText primary="Transaction" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton selected={selectedIndex === 2} onClick={() => setSelectedIndex(2)} component="a" href="/">
+              <ListItemIcon>
+                <AbcIcon />
+              </ListItemIcon>
+              <ListItemText primary="Raw Transaction" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton selected={selectedIndex === 3} onClick={() => setSelectedIndex(3)} component="a" href="/">
+            <ListItemIcon>
+                <CurrencyBitcoinIcon />
+              </ListItemIcon>
+              <ListItemText primary="ERC20" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton selected={selectedIndex === 4} onClick={() => setSelectedIndex(4)} component="a" href="/">
+            <ListItemIcon>
+                <CurrencyBitcoinIcon />
+              </ListItemIcon>
+              <ListItemText primary="ERC721" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton selected={selectedIndex === 5} onClick={() => setSelectedIndex(5)} component="a" href="/">
+            <ListItemIcon>
+                <CurrencyBitcoinIcon />
+              </ListItemIcon>
+              <ListItemText primary="ERC1155" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton component="a" href="https://chainlist.org">
+              <ListItemIcon>
+                <LinkIcon />
+              </ListItemIcon>
+              <ListItemText primary="chainlist.org" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Stack>
+    </Paper>
+  </Bar>
+}
