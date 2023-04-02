43e763078dfbc51cd5d5a7175f0ecca85cb189a2
lolieatapple <lolieatapple@gmail.com>
2023-04-02 21:51:40 +0800

    Update instruction for sending private transactions in PrivateTx.js. Specify that only Wan Wallet Desktop v1.5.10 or above can receive the WAN and prompt users to ensure recipient has upgraded before sending

--- pages/privateTx.js
+++ pages/privateTx.js
@@ -148,7 +148,7 @@ export default function PrivateTx() {
 
       <h1>Multiple Private Transaction</h1>
       <h4>* Support batch sending of private transactions on Wanchain mainnet and testnet. Each line should contain a private address and the amount to be sent, separated by a comma. The amount must be a multiple of 10 WAN.</h4>
-      <h4>* Only Wan Wallet Desktop v1.5.10 or above can receive the file. Please ensure the recipient has upgraded to the corresponding version before sending. If not, the wanOTA_mainnet.json file needs to be manually deleted to trigger a new OTA address scan.</h4>
+      <h4>* Only Wan Wallet Desktop v1.5.10 or above can receive the WAN. Please ensure the recipient has upgraded to the corresponding version before sending. If not, the wanOTA_mainnet.json file needs to be manually deleted to trigger a new OTA address scan.</h4>
       <TextareaAutosize
         fullwidth="true"
         aria-label="minimum height"
