ad09758d52c6d992b5f14a4883af81ac72a78ceb
lolieatapple <lolieatapple@gmail.com>
2023-05-10 15:05:35 +0800

    remove gather code

--- pages/_sideBar.js
+++ pages/_sideBar.js
@@ -93,12 +93,6 @@ export default function SideBar() {
     }
   }
 
-  const gatherCode = `
-  <script id="gather_config" type="text/javascript">
-    window.Gather_SiteID = "d3633223-9130-4b15-8b59-8be93e3ef527";
-  </script>
-  <script id="gather_script" src="https://online.gather.network/gather.app.js"></script>
-  `;
   return <Bar>
     <Head>
       <title>Aries Web Wallet</title>
@@ -110,7 +104,6 @@ export default function SideBar() {
     </Head>
     <Paper elevation={12} sx={{minHeight: "100vh", overflow: 'auto'}}>
       <Stack spacing={0}>
-      <div id="gather" dangerouslySetInnerHTML={{__html: gatherCode}} />
       <Stack spacing={2} sx={{padding: '20px 15px'}}>
         <Stack spacing={1} direction='row' sx={{padding: '10px 0 10px 25px'}}>
         <Image alt="logo" src="/logo.png" width={180} height={58} style={{cursor:'pointer'}} onClick={()=>{
