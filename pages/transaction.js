5f750539e1a3c635b81ab1c3e082e4512a0a3ee6
lolieatapple <lolieatapple@gmail.com>
2023-05-30 18:07:41 +0800

    add send tx from private key

--- pages/transaction.js
+++ pages/transaction.js
@@ -1,6 +1,7 @@
 import { Button, Paper, Stack, TextField } from "@mui/material";
 import { useState } from "react";
 import useWallet from "./hooks/useWallet";
+import { ethers } from "ethers";
 
 export default function SendTransaction() {
   const [toAddr, setToAddr] = useState('');
@@ -10,6 +11,7 @@ export default function SendTransaction() {
   const [txHash, setTxHash] = useState('');
   const [txJson, setTxJson] = useState('');
   const [txReceipt, setTxReceipt] = useState('');
+  const [privateKey, setPrivateKey] = useState('');
   const web3 = wallet && wallet.web3;
   return <div>
     <Paper sx={{padding: '30px', margin: '50px', overflow:'auto'}}>
@@ -63,6 +65,34 @@ export default function SendTransaction() {
       <pre>{txJson}</pre>
       <h4>Tx Receipt</h4>
       <pre>{txReceipt}</pre>
+    </Stack>
+  </Paper>
+  <Paper sx={{padding: '30px', margin: '50px', overflow:'auto'}}>
+    <Stack spacing={2}>
+      <h1>Send Normal Transaction From Private Key</h1>
+      <TextField label="Private Key" type="password" value={privateKey} onChange={e=>setPrivateKey(e.target.value)} />
+      <TextField label="To Address" value={toAddr} onChange={e=>setToAddr(e.target.value)} />
+      <TextField label="Value in Ether" value={value} onChange={e=>setValue(e.target.value)} />
+      <TextField label="Data" value={data} onChange={e=>setData(e.target.value)} />
+      <Button variant="contained" color="primary" onClick={async ()=>{
+        try {
+          if (!web3) return;
+          const provider = new ethers.providers.Web3Provider(web3.currentProvider);
+          const wallet = new ethers.Wallet(privateKey, provider);
+          const tx = await wallet.sendTransaction({
+            from: wallet.address,
+            to: toAddr ? toAddr : undefined,
+            value: value ? ethers.utils.parseEther(value) : '0',
+            data: data
+          });
+          
+          console.log(tx);
+          window.alert(`Transaction Hash: ${tx.hash}`);
+        } catch (error) {
+          console.error(error);
+          window.alert(error.message);
+        }
+      }}>Send</Button>
     </Stack>
   </Paper>
     </div>
